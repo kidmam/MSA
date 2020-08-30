b8aea16b3af2a978fcb41f8c2be0abb98e9f7b32
SangKil Park <ht1911@gmail.com>
2020-08-31 05:57:09 +0900

    Create migrations file

---
+++ migrations/migrations.go
@@ -0,0 +1,28 @@
+package migrations
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/kidmam/MSA/helpers"
+)
+
+type User struct {
+	gorm.Model
+	Username string
+	Email    string
+	Password string
+}
+
+type Account struct {
+	gorm.Model
+	Type    string
+	Name    string
+	Balance uint
+	UserID  uint
+}
+
+func connectDB() *gorm.DB {
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=user dbname=dbname password=password sslmode=disable")
+	helpers.HandleErr(err)
+	return db
+}
